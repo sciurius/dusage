7a8a421c8252ee58031b74d181370d879b289911
Johan Vromans <jvromans@squirrel.nl>
1990-01-17 21:53:37 +0000

    add -a, $subtitle, !excl
    SCCS-vsn: 1.5

--- dusage.pl
+++ dusage.pl
@@ -13,12 +13,13 @@ $my_name = $0;
 
 sub usage {
   local ($help) = shift (@_);
-  local ($usg) = "usage: $my_name [-fghruD][-i input][-p dir] ctlfile";
+  local ($usg) = "usage: $my_name [-afghruD][-i input][-p dir] ctlfile";
   die "$usg\nstopped" unless $help;
   print STDERR "$usg\n";
   print STDERR <<EndOfHelp
 
     -D          - provide debugging info
+    -a          - provide all statis
     -f          - also report file statistics
     -g          - gather new data
     -h          - this help message
@@ -52,14 +53,16 @@ sub do_get_options {
   $retain = 0;
   $gather = 0;
   $allfiles = 0;
+  $allstats = 0;
 
   # Command line options. We use a modified version of getopts.pl.
 
-  &usage (0) if &Getopts ("Dfghi:p:ru");
+  &usage (0) if &Getopts ("Dafghi:p:ru");
   &usage (1) if $opt_h;
   &usage (0) if $#ARGV > 0;
 
   $debug    |= $opt_D if defined $opt_D;	# -D -> debug
+  $allstats |= $opt_a if defined $opt_a;	# -a -> all stats
   $allfiles |= $opt_f if defined $opt_f;	# -f -> report all files
   $gather   |= $opt_g if defined $opt_g;	# -g -> gather new data
   $retain   |= $opt_r if defined $opt_r;	# -r -> retain old entries
@@ -77,12 +80,13 @@ sub do_get_options {
   $table    = ($#ARGV == 0) ? shift (@ARGV) : "$prefix.du.ctl";
   $runtype = $allfiles ? "file" : "directory";
   if ($debug) {
-    print STDERR "@(#)@ dusage	1.4 - dusage.pl\n";
+    print STDERR "@(#)@ dusage	1.5 - dusage.pl\n";
     print STDERR "Options:";
     print STDERR " debug" if $debug;	# silly, isn't it...
     print STDERR $noupdate ? " no" : " ", "update";
     print STDERR $retain ? " " : " no", "retain";
     print STDERR $gather ? " " : " no", "gather";
+    print STDERR $allstats ? " " : " no", "allstats";
     print STDERR "\n";
     print STDERR "Root = $root [prefix = $prefix]\n";
     print STDERR "Control file = $table\n";
@@ -100,6 +104,7 @@ sub do_parse_ctl {
   # and the values dereived from previous runs.
   # The names of the directories are relative to the $root.
   # The name may contain '*' or '?' characters, and will be globbed if so.
+  # An entry starting with ! is excluded.
   #
   # To add a new dir, just add the name. The special name '.' may 
   # be used to denote the $root directory. If used, '-p' must be
@@ -124,8 +129,18 @@ sub do_parse_ctl {
     # allowable	      <dir><TAB><size> <size> ...
     # possible	      <dir>
 
-    if ( $tb eq "-" ) {		# break
-      push (@targets, "-");
+    if ( $tb =~ /^-/ ) {	# break
+      push (@targets, "$tb");
+      printf STDERR "tb: *break* $tb\n" if $debug;
+      next;
+    }
+
+    if ( $tb =~ /^!/ ) {	# exclude
+      $excl = $';		#';
+      @a = grep ($_ ne $excl, @targets);
+      @targets = @a;
+      push (@targets, "*$tb");
+      printf STDERR "tb: *excl* $tb\n" if $debug;
       next;
     }
 
@@ -149,7 +164,7 @@ sub do_parse_ctl {
     }
 
     # Check for globs ...
-    if ( $name =~ /\*|\?/ ) {
+    if ( $gather && $name =~ /\*|\?/ ) {
       print STDERR "glob: $name\n" if $debug;
       foreach $n ( <${name}> ) {
 	next unless $allfiles || -d $n;
@@ -182,7 +197,7 @@ sub do_gather {
   @list = ();
   $last = "///";
   foreach $name (sort (@targets)) {
-    next if $name eq "-" || ord($name) == ord("*");
+    next if $name =~ /^[-*]/;
     next unless $allfiles || -d $name;
     $targets .= "$name//"; 
     next if ($name =~ m|^$last/|);
@@ -218,8 +233,8 @@ sub do_gather {
 # Report generation
 
 format std_hdr =
-Disk usage statistics                      @<<<<<<<<<<<<<<<
-$date
+Disk usage statistics@<<<<<<<<<<<<<<<<<<<<<@<<<<<<<<<<<<<<<
+$subtitle, $date
 
  blocks    +day     +week  @<<<<<<<<<<<<<<<
 $runtype
@@ -228,6 +243,19 @@ $runtype
 format std_out =
 @>>>>>> @>>>>>>> @>>>>>>>  ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<..
 $blocks, $d_day, $d_week, $name
+.
+
+format all_hdr =
+Disk usage statistics@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<           @<<<<<<<<<<<<<<<
+$subtitle, $date
+
+ --0--    --1--    --2--    --3--    --4--    --5--    --6--    --7--   @<<<<<<<<<<<<<<<
+$runtype
+-------  -------  -------  -------  -------  -------  -------  -------  --------------------------------
+.
+format all_out =
+@>>>>>> @>>>>>>> @>>>>>>> @>>>>>>> @>>>>>>> @>>>>>>> @>>>>>>> @>>>>>>>  ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<..
+$a[0],  $a[1],   $a[2],   $a[3],   $a[4],   $a[5],   $a[6],   $a[7],    $name
 .
 
 sub do_report_and_update {
@@ -240,17 +268,24 @@ sub do_report_and_update {
     }
   }
 
-  $^ = "std_hdr";
-  $~ = "std_out";
+  if ( $allstats ) {
+    $^ = "all_hdr";
+    $~ = "all_out";
+  }
+  else {
+    $^ = "std_hdr";
+    $~ = "std_out";
+  }
   $date = `date`;
   chop ($date);
 
   # In one pass the report is generated, and the control file rewritten.
 
   foreach $name (@targets) {
-    if ($name eq "-") {
-      print tb "-\n" unless $noupdate;
-      print STDERR "tb: -\n" if $debug;
+    if ($name =~ /^-/ ) {
+      $subtitle = $';				#';
+      print tb "$name\n" unless $noupdate;
+      print STDERR "tb: $name\n" if $debug;
       $- = -1;
       next;
     }
@@ -280,15 +315,17 @@ sub do_report_and_update {
     print STDERR "tb: $line" if $debug;
 
     $blocks = $a[0];
-    $d_day = $d_week = "";
-    if ($blocks ne "") {
-      if ($a[1] ne "") {		# dayly delta
-	$d_day = $blocks - $a[1];
-	$d_day = "+" . $d_day if $d_day > 0;
-      }
-      if ($a[7] ne "") {		# weekly delta
-	$d_week = $blocks - $a[7];
-	$d_week = "+" . $d_week if $d_week > 0;
+    if ( !$allstats ) {
+      $d_day = $d_week = "";
+      if ($blocks ne "") {
+	if ($a[1] ne "") {		# dayly delta
+	  $d_day = $blocks - $a[1];
+	  $d_day = "+" . $d_day if $d_day > 0;
+	}
+	if ($a[7] ne "") {		# weekly delta
+	  $d_week = $blocks - $a[7];
+	  $d_week = "+" . $d_week if $d_week > 0;
+	}
       }
     }
     write;
